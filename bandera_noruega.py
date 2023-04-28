57270f7321fef38b0cd6fc476a260221b636385a
omartorres28 <omartorres@colegioguanenta.edu.co>
2023-04-28 07:04:59 -0500

    Se creo la bandera de Noruega

---
+++ bandera_noruega.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+# Bandera De Francia
+ventana_principal = Tk()
+
+# ventana
+ventana_principal.title("Bandera de Francia")
+ventana_principal.geometry("800x800")
+ventana_principal.resizable(False, False)
+ventana_principal.config(bg="white")
+
+# frame rojo
+Frame_rojo = Frame(ventana_principal)
+Frame_rojo.config(bg="red", width=200, height=800)
+Frame_rojo. place(x=0, y=0)
+
+# frame blanco
+Frame_blanco = Frame(ventana_principal)
+Frame_blanco.config(bg="white", width=800, height=200)
+Frame_blanco.place(x=0, y=270)
+
+
+# frame rojo
+Frame_rojo = Frame(ventana_principal)
+Frame_rojo.config(bg="red", width=400, height=800)
+Frame_rojo. place(x=400, y=0)
+
+# frame blanco
+Frame_blanco = Frame(ventana_principal)
+Frame_blanco.config(bg="white", width=800, height=200)
+Frame_blanco.place(x=0, y=270)
+
+# frame azul
+Frame_azul= Frame(ventana_principal)
+Frame_azul.config(bg="blue4", width=800, height=100)
+Frame_azul.place(x=0, y=320)
+
+# frame azul
+Frame_azul= Frame(ventana_principal)
+Frame_azul.config(bg="blue4", width=100, height=800)
+Frame_azul.place(x=250, y=0)
+
+# run
+ventana_principal.mainloop()
